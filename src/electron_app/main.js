24aee7cad3d280978699caf48508cd4ad01898b6
Raphael <raphaelcst8@gmail.com>
2023-08-15 00:58:00 +0100

    Add electron_app and package files

---
+++ src/electron_app/main.js
@@ -0,0 +1,8 @@
+// const { BrowserWindow } = require('electron');
+// const { dirname } = require('path');
+
+// const win = new BrowserWindow({ width: 800, height: 600 });
+
+// win.loadFile('front/index.html');
+
+console.log("Hi");
