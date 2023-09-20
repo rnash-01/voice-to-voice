4ae315703b94a57af7781bda51654f836f595c22
Raphael <raphaelcst8@gmail.com>
2023-09-20 18:44:23 +0100

    Test order of bytes in `BinaryTree`
    When `WAVHandler::load` is called, the file is accessed in sequence, and naturally
    follows the order of the bytes in the file anyway. Samples are added, one-by-one,
    to the `BinaryTree`, in the same order. This has now been tested for.
    See `WAVHandler_test.cpp` lines `47`-`70`.
    
    Related issues: #8

--- cpp/src/test/WAVHandler_test.cpp
+++ cpp/src/test/WAVHandler_test.cpp
@@ -16,6 +16,10 @@ TEST_F(WAVReaderTest, DoesOpenFile)
 TEST_F(WAVReaderTest, DoesReadData)
 {
 	BinaryTree b;
+	BYTE fileBuf[16];
+
+	BYTE* bBuf;
+	std::ifstream* is;
 
 	// First, choose '5' as the number of samples per binary tree item.
 	r->setSamplesPerBufItem(5);
@@ -38,5 +42,30 @@ TEST_F(WAVReaderTest, DoesReadData)
 	r->setSamplesPerBufItem(26723);
 	r->load(b);
 	EXPECT_NE(b.getSize(), 0);
-	EXPECT_EQ(b.getSize(), r->getFSize());	
+	EXPECT_EQ(b.getSize(), r->getFSize());
+
+	// Check that chunks of the WAV file match
+	// that of the Binary Tree.
+	b.clear();
+	ASSERT_EQ(b.getSize(), 0);
+
+	r->setSamplesPerBufItem(8);
+	r->load(b);
+	ASSERT_NE(b.getSize(), 0);
+
+	is = new std::ifstream("./test.wav", std::ifstream::binary);
+	is->seekg(44);	// skip over info and format chunk, which contains 36
+	// Bits per sample = 16 (2 * 8)
+	
+	for (int i = 0; i < r->getFSize(); i += 16)
+	{
+		is->read((char*)fileBuf, 16);
+		bBuf = b.readBetween(i, i + is->gcount() - 1);
+		
+		for (int j = 0; j < is->gcount(); j++)
+			ASSERT_EQ(bBuf[j], fileBuf[j]);
+
+		delete[] bBuf;
+	}
+	is->close();
 }
