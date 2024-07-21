70cd918a5a51f2b1e8e2886d20711d049ec9a891
Raphael Nash <raphaelcst8@gmail.com>
2024-07-22 00:22:35 +0100

    Update sandbox to test writer

--- cpp/src/sandbox.cpp
+++ cpp/src/sandbox.cpp
@@ -5,5 +5,15 @@
 */
 int main(int argc, char** argv)
 {
-	WAVWriter w("./test_out.wav");
+  WAVReader r("./test.wav");
+  BinaryTree b;
+  r.load(b);
+  std::cout << r.getSampRate() << r.getBitsPerSample() << r.getBlockAlign() << r.getBytesPerSec() << r.getNChannels() << std::endl;
+
+
+	WAVWriter w(r, "./test_out.wav");
+  std::cout << w.getSampRate() << w.getBitsPerSample() << w.getBlockAlign() << w.getBytesPerSec() << w.getNChannels() << std::endl;
+  w.load(b);
+
+
 }
