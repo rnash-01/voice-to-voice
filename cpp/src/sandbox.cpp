83a7ba0fc5ed31158f38d1306ee1501ac06748fa
Raphael Nash <raphaelcst8@gmail.com>
2024-08-09 18:57:12 +0100

    Update sandbox

--- cpp/src/sandbox.cpp
+++ cpp/src/sandbox.cpp
@@ -1,5 +1,6 @@
 #include <main.h>
-#include <task/BasicTask.h>
+// #include <task/BasicTask.h>
+#include <task/FunctionTask.h>
 #include <task/TaskPool.h>
 /*
   Sandbox - for debugging/experimenting
@@ -15,11 +16,10 @@ char addChar(char a, char b) {
 int main(int argc, char** argv)
 {
     // Instantiate tasks
-    std::unique_ptr<FunctionTask<int, std::string>> task1 = std::make_unique<FunctionTask<int, std::string>>();
-    task1->bind([](std::string str) -> int {
+    std::shared_ptr<FunctionTask<int, std::string>> task1 = std::make_shared<FunctionTask<int, std::string>>(([](std::string str) -> int {
         std::cout << "Task output: " << str << std::endl;
         return 0;
-    }, "HI");
+    }), "HI!");
     // FunctionTask<int, int, int> task2([](int a, int b) -> int {
     //     for (int i = 0; i < 500; i++) {
     //         std::cout << std::endl;
@@ -29,8 +29,9 @@ int main(int argc, char** argv)
     // FunctionTask<char, char, char> task3(addChar, 65, 1);
 
     // Create TaskPool
-    std::vector<std::unique_ptr<Task>> tasks;
-    tasks.insert(tasks.end(), std::make_unique<FunctionTask<int, std::string>>());
+    std::vector<std::shared_ptr<Task>> tasks = std::vector<std::shared_ptr<Task>>();
+    tasks.insert(tasks.end(), task1);
+    // tasks.insert(tasks.end(), std::make_unique<FunctionTask<int, std::string>>());
     // tasks.insert(tasks.end(), std::make_unique<FunctionTask<int, int, int>>(task2));
     // tasks.insert(tasks.end(), std::make_unique<FunctionTask<char, char, char>>(task3));
 
